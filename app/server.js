7ff6444bb88def844c78dfcd263e2d2fef07f0f4
raj68518 <raj68518@gmail.com>
2020-10-12 08:45:33 +0530

    port name changed

--- app/server.js
+++ app/server.js
@@ -4,13 +4,13 @@ const env = require('dotenv');
 const app = express();
 
 env.config();
-app.set('PORT', process.env.port || 3000);
+app.set('port', process.env.port || 3000);
 
 
 app.get("/", (req, res) =>{
     res.send("hello Aditya !");
 })
 
-app.listen(app.get('PORT'), () =>{
-    console.log(`server running.. on http://localhost:${app.get("PORT")}`);
+app.listen(app.get('port'), () =>{
+    console.log(`server running.. on http://localhost:${app.get("port")}`);
 })
