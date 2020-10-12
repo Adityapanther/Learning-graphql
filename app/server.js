22d9702c9aeeb8271070ea2f97751905e0bc180c
raj68518 <raj68518@gmail.com>
2020-10-12 11:20:56 +0530

    graphql added

--- app/server.js
+++ app/server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const path = require('path');
 const env = require('dotenv');
-const expressGraphQl = require('express-graphql');
-const graphQl = require('graphql');
+const {graphqlHTTP} = require('express-graphql');
+const {
+    graphql,
+    buildSchema
+} = require('graphql');
 
 const app = express();
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {
+    hello: () => 'Hello world!'
+};
+
+app.use('/graphql', new graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+    
+}))
+
 
 env.config();
 app.set('port', process.env.port || 3000);
@@ -16,4 +37,5 @@ app.get("/", (req, res) =>{
 
 app.listen(app.get('port'), () =>{
     console.log(`server running.. on http://localhost:${app.get("port")}`);
+    console.log(`GrapgQl running.. on http://localhost:${app.get("port")}/graphql`);
 })
