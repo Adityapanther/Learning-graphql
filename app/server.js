26d4f5e362622442d14104394e97febf162a8d73
raj68518 <raj68518@gmail.com>
2020-10-12 08:44:18 +0530

    dotenv file added

--- app/server.js
+++ app/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
+const env = require('dotenv');
 const app = express();
+
+env.config();
+app.set('PORT', process.env.port || 3000);
+
 
 app.get("/", (req, res) =>{
     res.send("hello Aditya !");
 })
 
-app.listen('3000', () =>{
-    console.log("server running.. on http://localhost:3000");
+app.listen(app.get('PORT'), () =>{
+    console.log(`server running.. on http://localhost:${app.get("PORT")}`);
 })
